d1dfa520e55dac673d891f375de99ec6e172aae2
Matthew Weber <mtwb47@gmail.com>
2025-08-01 20:17:46 +0200

    Initial commit

---
+++ vulcan/modules/filesystem.nix
@@ -0,0 +1,58 @@
+{ config, pkgs, ... }:
+
+{
+  # File System
+
+  fileSystems."/smssd" = {
+    device = "/dev/disk/by-uuid/3dbd2939-f90f-4551-aecb-c7d16217cab7";
+    fsType = "ext4";
+    options = [
+      "defaults"
+      "nofail"
+    ];
+  };
+
+  # NFS
+
+
+  services.nfs.server.enable = true;
+  services.nfs.server.exports = ''
+    /smssd *(rw,nohide,insecure,no_subtree_check,sync,all_squash,anonuid=1000,anongid=1000)
+  '';
+
+   # Allow NFS traffic through the firewall
+   networking.firewall.allowedTCPPorts = [ 2049 111 ];
+   networking.firewall.allowedUDPPorts = [ 2049 111 ];
+   networking.firewall.allowedTCPPortRanges = [
+     { from = 32765; to = 32769; }
+   ];
+   networking.firewall.allowedUDPPortRanges = [
+     { from = 32765; to = 32769; }
+   ];
+
+
+    fileSystems."/mnt/media" = {
+      device = "192.168.86.202:/mnt/media";
+      fsType = "nfs";
+      options = [ "x-systemd.automount" "noauto" "rw" "vers=4" ];
+    };
+
+    fileSystems."/mnt/stuff" = {
+      device = "192.168.86.202:/mnt/stuff";
+      fsType = "nfs";
+      options = [ "x-systemd.automount" "noauto" "rw" "vers=4" ];
+    };
+
+    fileSystems."/mnt/hub" = {
+      device = "192.168.86.202:/mnt/hub";
+      fsType = "nfs";
+      options = [ "x-systemd.automount" "noauto" "rw" "vers=4" ];
+    };
+
+    fileSystems."/mnt/backups" = {
+      device = "192.168.86.202:/backups";
+      fsType = "nfs";
+      options = [ "x-systemd.automount" "noauto" "rw" "vers=4" ];
+    };
+
+}
---
+++ workpc/flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Henrik's Flake";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+	nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
+    home-manager = {
+        url = "github:nix-community/home-manager/release-25.05";
+        inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { nixpkgs, nix-flatpak, home-manager, ... }@inputs:
+
+    {
+      nixosConfigurations = {
+        workpc = nixpkgs.lib.nixosSystem {          
+          modules = [
+            ./configuration.nix
+            ./modules/apps.nix
+            ./modules/services.nix
+            ./modules/filesystem.nix
+	    nix-flatpak.nixosModules.nix-flatpak
+	    home-manager.nixosModules.home-manager
+          ];
+          specialArgs = { inherit inputs; };
+        };
+        #vulcan = nixpkgs.lib.nixosSystem {
+          #modules = [
+            #./vulcan/configuration.nix
+            #./vulcan/modules/apps.nix
+            #./vulcan/modules/services.nix
+            #./vulcan/modules/filesystem.nix
+	    #nix-flatpak.nixosModules.nix-flatpak
+          #];
+          #specialArgs = { inherit inputs; };
+        #};
+      };
+    };
+}
