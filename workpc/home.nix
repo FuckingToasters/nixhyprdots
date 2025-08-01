a19f5942545d479c19eb7d28e9328a8afe3ab219
FuckingToasters <adiscord128+github@gmail.com>
2025-08-02 01:32:26 +0200

    Auto-commit: Sat Aug  2 01:32:26 AM CEST 2025

--- workpc/home.nix
+++ workpc/home.nix
@@ -4,7 +4,7 @@
   home.username = "henrikp";
   home.homeDirectory = "/home/henrikp";
   home.stateVersion = "25.05";
-
+  home.backupFileExtension = "hm-backup";
   home.packages = with pkgs; [
     noto-fonts
     noto-fonts-emoji
