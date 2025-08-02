85299df60e99cb4f775d7e724a9e673c76cc48c0
FuckingToasters <adiscord128+github@gmail.com>
2025-08-03 01:32:06 +0200

    Auto-commit: Sun Aug  3 01:32:06 AM CEST 2025

--- workpc/configuration.nix
+++ workpc/configuration.nix
@@ -13,7 +13,7 @@
   
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_lts;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   hardware.firmware = [ pkgs.linux-firmware ];
   hardware.firmwareCompression = "none";
   hardware.bluetooth.enable = true;
